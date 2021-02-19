65e935194746606c73f6b22adcc166b6e041d7a2
Osni Antônio Küchler <osni.antonio@gmail.com>
2021-02-18 21:14:11 -0300

    Grant all privileges user root

--- .docker/mysql/initdb.sql
+++ .docker/mysql/initdb.sql
@@ -1,2 +1,5 @@
 CREATE DATABASE IF NOT EXISTS code_micro_videos;
 CREATE DATABASE IF NOT EXISTS code_micro_videos_test;
+
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
