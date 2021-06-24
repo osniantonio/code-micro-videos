bf49d773b8ef0beda44dac2c96131f6f5bc944da
Osni Antônio Küchler <osni.antonio@gmail.com>
2021-06-24 10:43:28 -0300

    Ajuste na sequencia middleware CORS

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -13,14 +13,14 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $middleware = [
-        \App\Http\Middleware\Cors::class,
-        //\Fruitcake\Cors\HandleCors::class,
+    protected $middleware = [        
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \Fruitcake\Cors\HandleCors::class,
+        //\App\Http\Middleware\Cors::class,
     ];
 
     /**
@@ -54,7 +54,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'cors' => \App\Http\Middleware\Cors::class,
+        //'cors' => \App\Http\Middleware\Cors::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
