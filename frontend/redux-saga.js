e3e22d39c51e01e7070d9636f36ccaa20f8058d7
Osni Antônio Küchler <osni.antonio@gmail.com>
2021-07-21 18:00:21 -0300

    Uploads Sincronizados: Execução infinita nos Sagas

--- frontend/redux-saga.js
+++ frontend/redux-saga.js
@@ -1,6 +1,7 @@
 const { createStore, applyMiddleware } = require("redux");
 const { default: createDefaultMiddleware } = require("redux-saga");
-const { take, put } = require("redux-saga/effects");
+const { take, put, call } = require("redux-saga/effects");
+const axios = require("axios");
 
 function reducer(state, action) {
   if (action.type === "acaoX") {
@@ -8,19 +9,39 @@ function reducer(state, action) {
   }
 }
 
-function* helloWorld() {
+function* helloWorldSaga() {
   console.log("Hello World");
-  // fica parado até que capture essa ação
-  const action = yield take("acaoY");
-  yield put({
-    type: "acaoX",
-    value: "novo valor",
-  });
+  while (true) {
+    console.log("antes da acao y");
+    const action = yield take("acaoY");
+    const search = action.value;
+    try {
+      const { data } = yield call(
+        //() => axios.get('http://nginx/api/videos?search=' + search)
+        //(search) => axios.get('http://nginx/api/videos?search=' + search), search
+        axios.get, "http://nginx/api/videos?search=" + search
+      );
+      //console.log(data);
+      const value = "Novo valor ".concat(Math.random());
+      console.log(value);
+      yield put({
+        type: "acaoX",
+        value: data,
+      });
+    } catch (e) {
+        yield put({
+            type: "acaoE",
+            error: e,
+          });
+    }
+  }
 }
 
 const sagaMiddleware = createDefaultMiddleware();
 const store = createStore(reducer, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(helloWorld);
+sagaMiddleware.run(helloWorldSaga);
 const action = (type, value) => store.dispatch({ type, value });
 action("acaoY", "a");
+action("acaoY", "a");
+action("acaoW", "a");
 console.log(store.getState());
