e1353a22ffe5e10c04f3f3601799d0ebae6d0aa9
Osni Antônio Küchler <osni.antonio@gmail.com>
2021-07-21 16:21:27 -0300

    Uploads Sincronizados: Primeiro contato com Redux Saga

---
+++ frontend/redux-saga.js
@@ -0,0 +1,25 @@
+const { createStore, applyMiddleware } = require("redux");
+const { default: createDefaultMiddleware } = require("redux-saga");
+const { take, put } = require("redux-saga/effects");
+
+function reducer(state, action) {
+  if (action.type === "acaoX") {
+    return { value: action.value };
+  }
+}
+
+function* helloWorld() {
+  console.log("hello");
+  const action = yield take("acaoY");
+  yield put({
+    type: "acaoX",
+    value: "novo valor",
+  });
+}
+
+const sagaMiddleware = createDefaultMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(helloWorld);
+const action = (type, value) => store.dispatch({ type, value });
+action("acaoY", "a");
+console.log(store.getState());
