f42490a062d051b09164bbed18889220bed88f00
Osni Antônio Küchler <osni.antonio@gmail.com>
2021-07-22 09:46:01 -0300

    Uploads Sincronizados: Combinação de sagas e execuções de tarefas não bloqueantes

--- frontend/redux-saga.js
+++ frontend/redux-saga.js
@@ -1,6 +1,6 @@
 const { createStore, applyMiddleware } = require("redux");
 const { default: createDefaultMiddleware } = require("redux-saga");
-const { take, put, call, actionChannel, debounce, select } = require("redux-saga/effects");
+const { take, put, call, actionChannel, debounce, select, all, fork } = require("redux-saga/effects");
 const axios = require("axios");
 
 function reducer(state, action) {
@@ -10,6 +10,12 @@ function reducer(state, action) {
   if (action.type === "acaoX") {
     return { value: action.value };
   }
+}
+
+function* sagaNonBlocking() {
+  console.log("antes do call");
+  const {data} = yield call(axios.get, "http://nginx/api/videos");
+  console.log("depois do call");
 }
 
 function* searchData(action) {
@@ -22,15 +28,34 @@ function* searchData(action) {
     //const action = yield take(channel);
     const search = action.value;
     try {
+
+      yield fork(sagaNonBlocking);
+      console.log("depois do fork");
+      
+      // yield all mesma coisa do Promise.all
+      /*
+      const { response1, response2 } = yield all([
+        call(axios.get, "http://nginx/api/videos?search=" + search),
+        call(axios.get, "http://nginx/api/categories?search=" + search)
+      ]);
+      console.log(response1.data.data.length, response2.data.data.length);
+      */
+
+      /*
       const { data } = yield call(
         //() => axios.get('http://nginx/api/videos?search=' + search)
         //(search) => axios.get('http://nginx/api/videos?search=' + search), search
         axios.get, "http://nginx/api/videos?search=" + search
       );
+
+      const { data1 } = yield call(        
+        axios.get, "http://nginx/api/categories?search=" + search
+      );
+      */
       console.log(search);
       yield put({
         type: "acaoX",
-        value: data,
+        value: '',
       });
     } catch (e) {
         yield put({
@@ -45,9 +70,23 @@ function* debounceSearch() {
   yield debounce(1000, 'acaoY', searchData);
 }
 
+function* helloWorld() {
+  console.log('Hello World');
+}
+
+function* rootSaga() {
+  yield all([
+    helloWorld(),
+    debounceSearch()
+  ]);
+  //yield fork(helloWorld);
+  //yield fork(debounceSearch);
+  console.log('final');
+}
+
 const sagaMiddleware = createDefaultMiddleware();
 const store = createStore(reducer, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(debounceSearch);
+sagaMiddleware.run(rootSaga);
 const action = (type, value) => store.dispatch({ type, value });
 action("acaoY", "l");
 action("acaoY", "lu");
